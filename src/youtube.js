fe7aeef7df455bb5e564e5a8db8da10503d7778c
omkar <shelkeom230@gmail.com>
2024-03-19 18:57:14 +0530

    done setups and basics

---
+++ src/youtube.js
@@ -0,0 +1,11 @@
+
+function Youtube(){
+    return(
+        <>
+        <h1>Youtube react app</h1>
+        <h2>Coder omkar- frontend engineer here.</h2>
+        </>
+    );
+}
+
+export default Youtube
